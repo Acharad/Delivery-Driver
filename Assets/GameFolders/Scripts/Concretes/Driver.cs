96fed892e7662fbd292bd24b48eab818ce0ebd0b
Acharad <iahmetkavras@gmail.com>
2022-06-25 21:27:47 +0300

    End

--- Assets/GameFolders/Scripts/Concretes/Driver.cs
+++ Assets/GameFolders/Scripts/Concretes/Driver.cs
@@ -8,13 +8,22 @@ namespace GameFolders.Scripts.Concretes
 {
     public class Driver : MonoBehaviour
     {
-        [SerializeField] float rotationSpeed = 1f;
-        [SerializeField] float moveSpeed = 0.01f;
+        [SerializeField] float rotationSpeed = 125f;
+        [SerializeField] float moveSpeed = 20f;
+        private float slowSpeed;
+        private float boostSpeed;
 
         // private Rigidbody2D _rb;
 
         private float _rotationAmount;
         private float _moveAmount;
+
+        private void Awake()
+        {
+            slowSpeed = moveSpeed - 5f;
+            boostSpeed = moveSpeed + 5f;
+        }
+
         private void Start()
         {
             // gameObject.name = "McQueen";
@@ -27,15 +36,26 @@ namespace GameFolders.Scripts.Concretes
             _moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
             //_movement = new Vector3(0, moveAmount, rotationAmount);
-
-            
             
             transform.Rotate(0, 0,-_rotationAmount);
             transform.Translate(0, _moveAmount, 0);
         }
 
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Boost"))
+            {
+                moveSpeed = boostSpeed;
+                Destroy(col.gameObject, 0.2f);
+            }
+        }
         
-
-
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Obstacle"))
+            {
+                moveSpeed = slowSpeed;
+            }
+        }
     }
 }
